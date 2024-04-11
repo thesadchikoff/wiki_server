bafe67f3c92e79678a0299e5e09074064fe1b710
thesadchikoff <bruni.glat@yandex.ru>
2024-04-11 10:45:16 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT ?? 3000;
-  app.enableCors();
+  app.enableCors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  });
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
