c9f0602713405fbc6096a8b532114549cedd048f
thesadchikoff <bruni.glat@yandex.ru>
2024-04-11 10:47:41 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT ?? 3000;
   app.enableCors({
-    origin: ['*'],
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
