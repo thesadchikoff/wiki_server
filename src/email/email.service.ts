af2f24d0912b358fae1ea7262e5cee6162d3093c
thesadchikoff <bruni.glat@yandex.ru>
2024-05-08 14:42:36 +0300

    fix smtp errors

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -10,32 +10,40 @@ export class EmailService {
   ) {}
 
   async sendUserConfirmation(categoryId: string) {
-    const category = await this.prismaService.categories.findFirst({
-      where: {
-        id: categoryId,
-      },
-      include: {
-        moderators: true,
-      },
-    });
-    category.moderators.forEach(async (modrator) => {
-      await this.mailerService.sendMail({
-        to: modrator.email,
-        subject: 'Уведомление от WIKI',
-        template: './confirmation',
+    try {
+      const category = await this.prismaService.categories.findFirst({
+        where: {
+          id: categoryId,
+        },
+        include: {
+          moderators: true,
+        },
       });
-    });
+      category.moderators.forEach(async (modrator) => {
+        await this.mailerService.sendMail({
+          to: modrator.email,
+          subject: 'Уведомление от WIKI',
+          template: './confirmation',
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async sendMailForVerify(email: string, link: string) {
-    await this.mailerService.sendMail({
-      to: email,
-      subject: 'Регистрация в WIKI',
-      template: './verification',
-      context: {
-        link,
-      },
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        subject: 'Регистрация в WIKI',
+        template: './verification',
+        context: {
+          link,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async sendEmailForAcceptPublishUser(
@@ -44,16 +52,19 @@ export class EmailService {
     noteId: string,
     type: boolean = true,
   ) {
-    // /categories/832607da-8f87-476c-aade-99d8a3f737b9/note/793e6323-5702-466d-9c5d-d39fe8f8a85b
-    await this.mailerService.sendMail({
-      to: email,
-      subject: 'Уведомление от WIKI',
-      template: './confirm',
-      context: {
-        categoryId,
-        noteId,
-        accept: type,
-      },
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        subject: 'Уведомление от WIKI',
+        template: './confirm',
+        context: {
+          categoryId,
+          noteId,
+          accept: type,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
